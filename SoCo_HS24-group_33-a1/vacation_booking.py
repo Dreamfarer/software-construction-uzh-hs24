9c66a23bfe1cd8274df510bf9544b7a058028625
Ceyhun Acikmese <ceyhunemrebey@hotmail.com>
2024-10-07 09:35:14 +0200

    feat: implement constructor

--- SoCo_HS24-group_33-a1/vacation_booking.py
+++ SoCo_HS24-group_33-a1/vacation_booking.py
@@ -2,3 +2,12 @@ VacationPackage = {
     "_classname": "VacationPackage",
     "_parent": None
 }
+
+def vacation_package_new(destination: str, cost_per_day: int, duration_in_days: int):
+    new_object = {
+        "destination": destination,
+        "cost_per_day": cost_per_day,
+        "duration_in_days": duration_in_days,
+        "_class": VacationPackage
+    }
+    return new_object
