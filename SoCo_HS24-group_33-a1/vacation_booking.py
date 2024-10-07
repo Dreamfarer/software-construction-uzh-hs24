9371fadeaa612f4b2a1fc58edcafe3838bb84c08
Mischa Jampen <mischa.jampen@gmail.com>
2024-10-07 18:09:39 +0200

    feat: implementation constructor

--- SoCo_HS24-group_33-a1/vacation_booking.py
+++ SoCo_HS24-group_33-a1/vacation_booking.py
@@ -5,3 +5,13 @@ BeachResort = {
     "cost": calculate_cost,
     "package": describe_package
 }
+
+def beach_resort_new(destination, cost_per_day, duration, surfing) -> dict:
+    new_vacation = {
+        "destination": destination,
+        "cost_per_day": cost_per_day,
+        "duration": duration,
+        "surfing": surfing,
+        "_class": BeachResort
+    }
+    return new_vacation
