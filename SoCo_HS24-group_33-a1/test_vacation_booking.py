38442c1a926e5ecc71691af9e94b6e62658bfd53
Ceyhun Acikmese <ceyhunemrebey@hotmail.com>
2024-10-16 21:40:04 +0200

    feat: add calculate cost test for each class

--- SoCo_HS24-group_33-a1/test_vacation_booking.py
+++ SoCo_HS24-group_33-a1/test_vacation_booking.py
@@ -55,3 +55,55 @@ def create_sample_vacations() -> list:
         has_private_suite=False,
     )
     return [beach_resort, adventure_trip, luxury_cruise]
+
+
+@prevent_append
+def test_beachresort_calculatecost():
+    beach_resort = new(
+        BeachResort,
+        destination="Maldives",
+        cost_per_day=100,
+        duration_in_days=7,
+        include_surfing=True,
+    )
+    actual = call(beach_resort, "calculate_cost")
+    expected = 7 * 100 + 100
+    assert actual == expected
+
+@prevent_append
+def test_adventuretrip_calculatecost():
+    adventure_trip = new(
+        AdventureTrip,
+        destination="Macchu Picchu",
+        cost_per_day=150,
+        duration_in_days=4,
+        difficulty_level="easy",
+    )
+    actual = call(adventure_trip, "calculate_cost")
+    expected = 150 * 4
+    assert actual == expected
+
+@prevent_append
+def test_luxury_cruise_calculatecost():
+    luxury_cruise = new(
+        LuxuryCruise,
+        destination="Mediterranean",
+        cost_per_day=100,
+        duration_in_days=14,
+        has_private_suite=False,
+    )
+    actual = call(luxury_cruise, "calculate_cost")
+    expected = 100 * 14
+    assert actual == expected
+
+@prevent_append
+def test_vacationbookingsummary_calculatecost():
+    create_sample_vacations()
+    vacation_booking_summary = new(VacationBookingSummary)
+    actual = call(vacation_booking_summary, "calculate_cost")
+    expected = (7 * 100 + 100) + (150 * 4) + (100 * 14)
+    assert actual == expected
+
+if __name__ == "__main__":
+    tests = find_tests()
+    run_tests(tests)
