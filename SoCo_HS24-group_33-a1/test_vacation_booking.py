17fc1198f90db63c37b791632347643d670eb8bc
Ceyhun Acikmese <ceyhunemrebey@hotmail.com>
2024-10-17 12:55:14 +0200

    feat: implement suggestions of !14

--- SoCo_HS24-group_33-a1/test_vacation_booking.py
+++ SoCo_HS24-group_33-a1/test_vacation_booking.py
@@ -1,7 +1,8 @@
-from vacation_booking import * 
-import time 
+from vacation_booking import *
+import time
 
-def run_tests(all_tests: list) -> None:
+
+def run_tests(all_tests: list[Callable]) -> None:
     """
     Runs each test in the list all_tests, measures the time taken for each tests, and prints
     the results (pass, fail or error) along with the time taken.
@@ -15,31 +16,36 @@ def run_tests(all_tests: list) -> None:
     results = {"pass": 0, "fail": 0, "error": 0}
 
     for test in all_tests:
+        global booked_vacations
+        booked_vacations.clear()
         start_time = time.time()
         total_time = 0
+
         try:
             test()
-            elapsed_time = time.time() - start_time
             results["pass"] += 1
-            print(f"{test.__name__}: pass {elapsed_time:.3f} seconds")
+            result = "pass"
         except AssertionError:
-            elapsed_time = time.time() - start_time
             results["fail"] += 1
-            print(f"{test.__name__}: fail {elapsed_time:.3f} seconds")
+            result = "fail"
         except Exception as e:
-            elapsed_time = time.time() - start_time
             results["error"] += 1
-            print(f"{test.__name__}: error {e} {elapsed_time:.3f} seconds")
+            result = f"error {e}"
         finally:
+            elapsed_time = time.time() - start_time
+            print(f"{test.__name__}: {result} {elapsed_time:.3f} seconds")
             total_time += elapsed_time
-    
-    print("------------------------------------------------\n"
-          f"Ran {len(all_tests)} tests in {total_time:.3f}s\n"
-          f"pass {results['pass']}\n"
-          f"fail {results['fail']}\n"
-          f"error {results['error']}")
 
-def find_tests(prefix: str = "test_") -> list:
+    print(
+        "------------------------------------------------\n"
+        f"Ran {len(all_tests)} tests in {total_time:.3f}s\n"
+        f"pass {results['pass']}\n"
+        f"fail {results['fail']}\n"
+        f"error {results['error']}"
+    )
+
+
+def find_tests(prefix: str = "test_") -> list[Callable]:
     """
     Finds all test functions whose names start with a given prefix.
 
@@ -53,36 +59,13 @@ def find_tests(prefix: str = "test_") -> list:
     tests = []
     for name, func in globals().items():
         if name.startswith(prefix):
-           tests.append(func)
+            tests.append(func)
     return tests
 
-def prevent_append(func: Callable) -> Callable:
+
+def create_sample_vacations() -> None:
     """
-    Prevents appending an object to the global "booked_vacations" list during the execution of a function.
-
-    Args:
-        func (Callable): The function object in which we don't want to change "booked_vacations".
-
-    Returns:
-        Callable: The updated function with more functionality to prevent appending.
-    """
-    def wrapper(*args, **kwargs):
-        original = globals()["booked_vacations"].copy()
-        globals()['booked_vacations'].clear()
-        try:
-            func(*args, **kwargs)
-        finally:
-            globals()["booked_vacations"].extend(original)
-        
-    wrapper.__name__ = func.__name__
-    return wrapper
-
-def create_sample_vacations() -> list:
-    """
-    Creates sample vacation objects (BeachResort, AdventureTrip, LuxuryCruise) to reduce repetitive code.
-
-    Returns:
-        list: A list of instantiated vacation objects (BeachResort, AdventureTrip, LuxuryCruise).
+    Instantiate and append sample vacation objects (BeachResort, AdventureTrip, LuxuryCruise) to the global 'booked_vacations' list.
     """
     beach_resort = new(
         BeachResort,
@@ -105,10 +88,8 @@ def create_sample_vacations() -> list:
         duration_in_days=14,
         has_private_suite=False,
     )
-    return [beach_resort, adventure_trip, luxury_cruise]
 
 
-@prevent_append
 def test_beachresort_calculatecost():
     """
     Tests the calculate_cost method for a BeachResort instance with surfing included.
@@ -124,7 +105,7 @@ def test_beachresort_calculatecost():
     expected = 7 * 100 + 100
     assert actual == expected
 
-@prevent_append
+
 def test_adventuretrip_calculatecost():
     """
     Tests the calculate_cost method for a AdventureTrip instance with an easy difficulty level.
@@ -140,7 +121,7 @@ def test_adventuretrip_calculatecost():
     expected = 150 * 4
     assert actual == expected
 
-@prevent_append
+
 def test_luxury_cruise_calculatecost():
     """
     Tests the calculate_cost method for a LuxuryCruise instance without a private suite.
@@ -156,7 +137,7 @@ def test_luxury_cruise_calculatecost():
     expected = 100 * 14
     assert actual == expected
 
-@prevent_append
+
 def test_vacationbookingsummary_calculatecost():
     """
     Tests the calculate_cost method for VacationBookingSummary without passing "search_term".
@@ -164,8 +145,10 @@ def test_vacationbookingsummary_calculatecost():
     create_sample_vacations()
     vacation_booking_summary = new(VacationBookingSummary)
     actual = call(vacation_booking_summary, "calculate_cost")
+    print(actual)
     expected = (7 * 100 + 100) + (150 * 4) + (100 * 14)
     assert actual == expected
+
 
 if __name__ == "__main__":
     tests = find_tests()
