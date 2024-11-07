4d87cf7f26f8135779f09441502e20d8035aba69
Mischa Jampen <mischa.jampen@gmail.com>
2024-11-07 20:27:48 +0100

    refactor: change data structure and name for 'time'

--- SoCo_HS24-group_33-a2/reporting.py
+++ SoCo_HS24-group_33-a2/reporting.py
@@ -2,24 +2,24 @@ import sys
 from datetime import datetime
 
 def parse_log(log_file):
-    time = {}
+    data = {}
     with open(log_file, "r") as file:
         for line in file:
             if line.startswith("id"):
                 continue
             id, timestamp,function_name,event = line.strip().split(",")
             timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
-            if id not in time:
-                time[id] = [0]
-            if event == "start" and id in time:
-                time[id].append(function_name)
-                time[id].append(timestamp)
-                time[id][0] += 1
-            if event == "stop" and id in time:
-                total_time = round((timestamp - time[id][2]).total_seconds() * 1000,3)
-                time[id][2] = total_time
-                time[id][0] += 1 
-    return time
+            if id not in data:
+                data[id] = {"function_name": None, "calls": 0, "start_time": None, "total_time": 0}
+            if event == "start":
+                data[id]["function_name"] = function_name
+                data[id]["start_time"] = timestamp
+                data[id]["calls"] += 1
+            if event == "stop" and data[id]["start_time"] is not None:
+                total_time = round((timestamp - data[id]["start_time"]).total_seconds() * 1000,3)
+                data[id]["total_time"] = total_time
+                data[id]["calls"] += 1
+    return data
 
 def print_logs(logs):
     pass
