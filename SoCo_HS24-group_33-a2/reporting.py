68b12ee72bd1a6d700ec48e589f5cec37189dfaf
Mischa Jampen <mischa.jampen@gmail.com>
2024-11-07 19:51:06 +0100

    feat: calculate time from timestamp

--- SoCo_HS24-group_33-a2/reporting.py
+++ SoCo_HS24-group_33-a2/reporting.py
@@ -1,20 +1,21 @@
-from datetime import datetime
 import sys
+from datetime import datetime
 
 def parse_log(log_file):
-
-    logs = {}
+    time = {}
     with open(log_file, "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            if not line.startswith("id"):
-                id, timestamp, fuction_name, event = line.split(",")
-                if id in logs and event == "stop":
-                    logs[id] = [fuction_name,timestamp,event]
-                else:
-                    logs[id] = [fuction_name,timestamp,event]
-
-    return logs
+        for line in file:
+            if line.startswith("id"):
+                continue
+            id, timestamp,function_name,event = line.split(",")
+            timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+            if event == "start":
+                time[id] = [function_name,timestamp]
+            if event == "stop" and id in time:
+                total_time = (timestamp - time[id][1]).total_seconds() * 1000
+                time[id][1] = total_time
+             
+    return time
 
 def print_logs(logs):
     pass
