dd49241109735acb9e670fb86eae1a7a7a1a2744
Ceyhun Acikmese <ceyhunemrebey@hotmail.com>
2024-11-10 18:26:32 +0100

    style: reformat the code

--- SoCo_HS24-group_33-a2/reporting.py
+++ SoCo_HS24-group_33-a2/reporting.py
@@ -4,26 +4,33 @@ from datetime import datetime
 BLUE = "\033[36m"
 RESET = "\033[0m"
 
+
 def parse_log(log_file) -> dict:
     data = {}
     with open(log_file, "r") as file:
         for line in file:
             if line.startswith("id"):
                 continue
-            id, timestamp,function_name,event = line.strip().split(",")
+            id, timestamp, function_name, event = line.strip().split(",")
             timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
             if id not in data:
-                data[id] = {"function_name": None, "calls": 0, "start_time": None, "total_time": 0}
+                data[id] = {
+                    "function_name": None,
+                    "calls": 0,
+                    "start_time": None,
+                    "total_time": 0,
+                }
             if event == "start":
                 data[id]["function_name"] = function_name
                 data[id]["start_time"] = timestamp
                 data[id]["calls"] += 1
             if event == "stop" and data[id]["start_time"] is not None:
-                elapsed_time = round((timestamp - data[id]["start_time"]).total_seconds() * 1000,3)
+                elapsed_time = round((timestamp - data[id]["start_time"]).total_seconds() * 1000, 3)
                 elapsed_time = "{:.3f}".format(elapsed_time)
-                data[id]["total_time"] = (elapsed_time)
+                data[id]["total_time"] = elapsed_time
                 data[id]["calls"] += 1
     return data
+
 
 def print_results(data: dict) -> None:
     print("\n")
@@ -33,7 +40,7 @@ def print_results(data: dict) -> None:
         name = value["function_name"]
         calls = value["calls"]
         total_time = value["total_time"]
-        average_time = round(float(total_time)/calls,3)
+        average_time = round(float(total_time) / calls, 3)
         average_time = "{:.3f}".format(average_time)
 
         print(f"|" + " " * 2 + name + " " * (16 - len(name)) +
