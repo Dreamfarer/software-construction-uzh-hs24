caa0a689ba59c7b2b2e997698bea77f803187bb3
Ceyhun Acikmese <ceyhunemrebey@hotmail.com>
2024-11-10 19:04:30 +0100

    refactor: generate additional dictionary from data for formatted output

--- SoCo_HS24-group_33-a2/reporting.py
+++ SoCo_HS24-group_33-a2/reporting.py
@@ -30,23 +30,38 @@ def parse_log(log_file: str) -> dict:
     return data
 
 
+def generate_function_stats(data: dict) -> dict:
+    function_stats = {}
+    for value in data.values():
+        function_name = value["function_name"]
+        calls = value["calls"]
+        total_time = value["total_time"]
+        if not function_name in function_stats:
+            function_stats[function_name] = {"calls": calls, "total_time": total_time}
+        else:
+            function_stats[function_name]["calls"] += calls
+            function_stats[function_name]["total_time"] += total_time
+    return function_stats
+
+
 def print_results(data: dict) -> None:
     print("\n")
     print(f"| {BLUE} Function Name  {RESET} |{BLUE} Num. of calls{RESET} | {BLUE} Total Time (ms) {RESET}| {BLUE}Average Time (ms) {RESET}|")
     print("|-------------------------------------------------------------------------|")
-    for value in data.values():
-        name = value["function_name"]
-        calls = value["calls"]
-        total_time = value["total_time"]
+
+    function_stats = generate_function_stats(data)
+    for function_name, stats in function_stats.items():
+        calls = stats["calls"]
+        total_time = stats["total_time"]
         average_time = total_time / calls
         total_time_formatted = f"{total_time:.3f}"
         average_time_formatted = f"{average_time:.3f}"
-
-        print(f"|" + " " * 2 + name + " " * (16 - len(name)) +
+        print(f"|" + " " * 2 + function_name + " " * (16 - len(function_name)) +
                "|" + " " * 7 + str(calls) + " " * (8 - len(str(calls)))+
                "|" + " " * 7 + str(total_time_formatted) + " " * (11 - len(str(total_time_formatted)))+
                "|" + " " * 7  +  str(average_time_formatted) + " " * (12 - len(average_time_formatted)) +
                "|")
+        
     print("|_________________________________________________________________________|")
     print("\n")
 
