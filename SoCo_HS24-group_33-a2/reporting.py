a60f96cbdc894bac32adb9021d81147f3624128d
Dreamfarer <dreamfarer@pm.me>
2024-11-11 17:53:54 +0100

    style: adjust printing 'print_results'

--- SoCo_HS24-group_33-a2/reporting.py
+++ SoCo_HS24-group_33-a2/reporting.py
@@ -16,7 +16,7 @@ def parse_log(log_file: str) -> dict:
     Returns:
         dict: A dictionary with the function name as key and another dictionary a the value containing the number of calls and the total time as keys.
     """
-    with open(log_file, 'r') as file:
+    with open(log_file, "r") as file:
         rows = sorted(list(csv.reader(file))[1:], key=lambda x: x[0])
         functions = {}
         for i in range(0, len(rows), 2):
@@ -24,7 +24,7 @@ def parse_log(log_file: str) -> dict:
             if name not in functions:
                 functions[name] = {"calls": 0, "total_time": timedelta(0)}
             start = datetime.strptime(rows[i][1], "%Y-%m-%d %H:%M:%S.%f")
-            stop = datetime.strptime(rows[i+1][1], "%Y-%m-%d %H:%M:%S.%f")
+            stop = datetime.strptime(rows[i + 1][1], "%Y-%m-%d %H:%M:%S.%f")
             functions[name]["calls"] += 1
             functions[name]["total_time"] += abs(stop - start)
     return functions
@@ -38,8 +38,9 @@ def print_results(data: dict) -> None:
     Args:
         data (dict): A dictionary with function call data parsed from the log file by 'parse_log'.
     """
-    print("\n")
-    print(f"| {BLUE} Function Name  {RESET} |{BLUE} Num. of calls{RESET} | {BLUE} Total Time (ms) {RESET}| {BLUE}Average Time (ms) {RESET}|")
+    print(
+        f"| {BLUE} Function Name  {RESET} |{BLUE} Num. of calls{RESET} | {BLUE} Total Time (ms) {RESET}| {BLUE}Average Time (ms) {RESET}|"
+    )
     print("|-------------------------------------------------------------------------|")
     for function_name, stats in data.items():
         calls = stats["calls"]
@@ -47,14 +48,11 @@ def print_results(data: dict) -> None:
         average_time = total_time / calls
         total_time_formatted = f"{total_time:.3f}"
         average_time_formatted = f"{average_time:.3f}"
-        print(f"|" + " " * 2 + function_name + " " * (16 - len(function_name)) +
-               "|" + " " * 7 + str(calls) + " " * (8 - len(str(calls)))+
-               "|" + " " * 7 + str(total_time_formatted) + " " * (11 - len(str(total_time_formatted)))+
-               "|" + " " * 7  +  str(average_time_formatted) + " " * (12 - len(average_time_formatted)) +
-               "|")
-        
-    print("|_________________________________________________________________________|")
-    print("\n")
+        name_row = f"|{' '  * 2}{function_name}{' ' * (16 - len(function_name))}"
+        calls_row = f"|{' '  * 7}{str(calls)}{' ' * (8 - len(str(calls)))}"
+        total_time_row = f"|{' '  * 7}{str(total_time_formatted)}{' ' * (11 - len(str(total_time_formatted)))}"
+        average_time_row = f"|{' '  * 7}{str(average_time_formatted)}{' ' * (12 - len(average_time_formatted))}|"
+        print(name_row + calls_row + total_time_row + average_time_row)
 
 
 def main() -> None:
